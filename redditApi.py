e3360b9c3efcfc02ca622df90e50f91dbf30ad9c
tugonbob <joshuakgao@gmail.com>
2022-06-04 00:19:24 -0500

    Reddit api working

--- redditApi.py
+++ redditApi.py
@@ -1,1 +1,14 @@
-print('Hi')
+import praw
+from praw.models import MoreComments
+
+reddit = praw.Reddit(client_id='z-5lJ-uWbatBGHSKW3NqiQ', client_secret='lhQnk7SNnS2EdlQz8bXnQe8uCqw6Eg', user_agent='YtLaughs')
+
+hot_posts = reddit.subreddit('AskReddit').hot(limit=1)
+
+for posts in hot_posts:
+    print(posts.title)
+    submission = reddit.submission(id=posts.id)
+    for comment in submission.comments:
+        if isinstance(comment, MoreComments):
+            continue
+        print(comment.body)
