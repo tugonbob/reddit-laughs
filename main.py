1e1ebe8069227810af02c0b5954583add4ab33da
tugonbob <joshuakgao@gmail.com>
2022-06-04 01:15:50 -0500

    Reddit post fetching workflow completed

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ reddit = redditApi.Reddit()
 
 posts = reddit.get_top_posts("AskReddit", 'week')
 
-title = "Top Posts"
+title = "Choose an Interesting Reddit Post"
 options = [post.title for post in posts]
+pickedPost, pickedPostIndex = pick(options, title)
 
-option, index = pick(options, title, indicator='=>')
-print(option, index)
+comments = reddit.get_top_comments(posts[pickedPostIndex])
+title = "Choose All Interesting Comments"
+options = [comment.body for comment in comments]
+chosenComments = [comment[0] for comment in pick(options, title, multiselect=True, min_selection_count=1)]
+print(chosenComments)
