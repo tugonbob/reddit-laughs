8403476aa8775d8380c129c2c09fd485a177690f
tugonbob <joshuakgao@gmail.com>
2022-06-04 01:01:38 -0500

    Favored 'pick' package over 'enquiries' package

--- main.py
+++ main.py
@@ -4,4 +4,9 @@ from pick import pick
 reddit = redditApi.Reddit()
 
 posts = reddit.get_top_posts("AskReddit", 'week')
-print(posts[0].title)
+
+title = "Top Posts"
+options = [post.title for post in posts]
+
+option, index = pick(options, title, indicator='=>')
+print(option, index)
